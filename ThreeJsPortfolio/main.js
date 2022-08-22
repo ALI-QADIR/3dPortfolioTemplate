d9e4ced00fa1397539b834d64a9ff08ac9063e30
ALI-QADIR <aliqadir786.is@gmail.com>
2022-08-22 09:37:05 +0530

    add avatar cube, moon and camera movement. udate CSS and HTML for scroll effects

--- ThreeJsPortfolio/main.js
+++ ThreeJsPortfolio/main.js
@@ -26,8 +26,9 @@ const renderer = new THREE.WebGL1Renderer({
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-// setting intial camera postion to 30 on Z axis
+// setting intial camera postion
 camera.position.setZ(30);
+camera.position.setX(-10);
 
 renderer.render(scene, camera);
 
@@ -55,7 +56,7 @@ scene.add(torus);
 // arguments are as follows, 1st color of light, 2nd intensity, 3rd distance (to which light will go) and decay
 const pointLight = new THREE.PointLight(0xffffff, 1, 1000, 1);
 // setting initial postion of the light
-pointLight.position.set(20, 20, 20);
+pointLight.position.set(5, 5, 5);
 
 // adding ambient light to the scene. it lights up the whole scene
 // arguments are as follows, 1st color of light and 2nd intensity
@@ -64,17 +65,18 @@ const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambientLight, pointLight);
 
 // adding light helper and grid helper to the scene to help better visualilse.
-const lightHelper = new THREE.PointLightHelper(pointLight); // point light helper
-const gridHelper = new THREE.GridHelper(200, 50); // grid helper
+
+// const lightHelper = new THREE.PointLightHelper(pointLight); // point light helper
+// const gridHelper = new THREE.GridHelper(200, 50); // grid helper
 /* GridHelper( size : Number, divisions : Number, colorCenterLine : Color, colorGrid : Color )
 size -- The size of the grid. Default is 10.
 divisions -- The number of divisions across the grid. Default is 10.
 colorCenterLine -- The color of the centerline. This can be a Color, a hexadecimal value and an CSS-Color name. Default is 0x444444
 colorGrid -- The color of the lines of the grid. This can be a Color, a hexadecimal value and an CSS-Color name. Default is 0x888888 */
-scene.add(lightHelper, gridHelper);
+// scene.add(lightHelper, gridHelper);
 
 // adding orbit controls
-const controls = new OrbitControls(camera, renderer.domElement); // listens DOM events on mouse and updates camera position
+// const controls = new OrbitControls(camera, renderer.domElement); // listens DOM events on mouse and updates camera position
 // add control.update in animate function to update UI.
 
 function addStar() {
@@ -96,6 +98,55 @@ Array(200).fill().forEach(addStar);
 const spaceTexture = new THREE.TextureLoader().load("space.jpg");
 scene.background = spaceTexture;
 
+// adding avatar cube
+const avatarTexture = new THREE.TextureLoader().load("avatar.jpg");
+const avatar = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshBasicMaterial({ map: avatarTexture })
+);
+scene.add(avatar);
+
+// adding moon
+const moonTexture = new THREE.TextureLoader().load("moon.jpg");
+const normalTexture = new THREE.TextureLoader().load("normal.jpg");
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: moonTexture,
+    normalMap: normalTexture,
+  })
+);
+
+scene.add(moon);
+
+moon.position.z = 30;
+moon.position.setX(-10);
+
+avatar.position.z = -5;
+avatar.position.x = 2;
+
+// adding camera movement on scroll
+function moveCamera() {
+  // gets the hegiht from the top to which the page is scrolled
+  const t = document.body.getBoundingClientRect().top;
+
+  // set moon rotation on scroll
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  avatar.rotation.y += 0.01;
+  avatar.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
+
 // recursive function to give an infinite loop to call render method automatically
 function animate() {
   requestAnimationFrame(animate); // a mechanism that tells the browser that you want to perform an animation
@@ -105,10 +156,11 @@ function animate() {
   torus.rotation.z += 0.01;
 
   // adding controls.update function
-  controls.update();
+  // controls.update();
 
   renderer.render(scene, camera); // Now when the browser repaints the screen it calls the render method and updates the UI just like Game loop in game dev
 }
 
 animate();
-// 56
+
+// 56-
