d15ec4be28913f70071233f9b24f53aacde9455a
ALI-QADIR <aliqadir786.is@gmail.com>
2022-08-23 11:45:44 +0530

    add mobile view fix

---
+++ ThreeJsPortfolio/index.js
@@ -0,0 +1,11 @@
+import app from "./main";
+
+if (window.innerWidth < window.innerHeight) {
+  var q = document.querySelector("#phone");
+  q.classList.remove("hide");
+  q.classList.add("potrait");
+  document.querySelector("#main").style.height = window.innerHeight;
+} else {
+  document.querySelector("#main").style.height = "5000px";
+  app();
+}
