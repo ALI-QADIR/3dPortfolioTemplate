2eaa375563a83057f7793b8f6e273d81e08e994a
Ali Qadir <94987050+ALI-QADIR@users.noreply.github.com>
2022-09-13 19:02:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 import app from "./main.js";
 
-if (window.innerWidth < window.innerHeight) {
-  var q = document.querySelector("#phone");
-  q.classList.remove("hide");
-  q.classList.add("potrait");
-  document.querySelector("#main").style.height = window.innerHeight;
-} else {
-  document.querySelector("#main").style.height = "5000px";
-  app();
-}
+document.querySelector("#main").style.height = "5000px";
+app();
